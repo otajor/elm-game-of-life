3d138c7cdde962723fb2bdb5efbb403c2d28e9b7
Owen TM <oturnermajor@gmail.com>
2016-12-06 00:39:14 +0000

    change opening configuration

--- src/Model.elm
+++ src/Model.elm
@@ -3,25 +3,23 @@ module Model exposing (..)
 type alias Cell = (Int, Int) -- denotes cell's location on board
 type alias Model =
   { boardSize : Int
-  , cellSize : Int
   , liveCells : List Cell
   }
 
 model =
   { boardSize = 30
-  , cellSize = 10
   , liveCells = -- live cells on start
-    [ (0,0)
-    , (0,1)
-    , (0,2)
-    , (0,3)
-    , (0,4)
-    , (2,0)
-    , (2,4)
-    , (4,0)
-    , (4,1)
-    , (4,2)
-    , (4,3)
-    , (4,4)
+    [ (-2, -2)
+    , (-2, -1)
+    , (-2, 0)
+    , (-2, 1)
+    , (-2, 2)
+    , (0, -2)
+    , (0, 2)
+    , (2, -2)
+    , (2, -1)
+    , (2, 0)
+    , (2, 1)
+    , (2, 2)
     ]
   }
